07aa20e0e028ad021041da6c33beea405b1a0932
Mitchell Cook <m.cook2434@gmail.com>
2020-11-09 17:33:02 -0600

    changed the routes.rb file to use RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/show'
-  get 'products/index'
-  get 'products/show'
+  # get 'categories/index'
+  # get 'categories/show'
+  # get 'products/index'
+  # get 'products/show'
+  resources :products, only: %i[index show]
+  resources :categories, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
