ee2632c2c56345cc8659f9774b25cbaf69b9b1e1
Mitchell Cook <m.cook2434@gmail.com>
2020-11-19 14:07:50 -0600

    added initialize_session method and cart helper method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_session
+  helper_method :cart
   helper_method :categories_for_select
 
   def categories_for_select
     categories = [Category.new(category_name: "All", id: -99), *Category.all]
+  end
+
+  private
+
+  def initialize_session
+    session[:shopping_cart] ||= [] # The shopping cart will be an array of product ids.
+  end
+
+  def cart
+    # Return a collection of Product objects based on the product ids in the shopping cart.
+    Product.find(params[:id])
   end
 end
