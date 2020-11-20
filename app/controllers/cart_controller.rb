19e0fefe050bee98418961afd8b31f2274c1346d
Mitchell Cook <m.cook2434@gmail.com>
2020-11-20 11:33:28 -0600

    added image to offest cart with mvp.css, put the cart into application layout

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -5,7 +5,9 @@ class CartController < ApplicationController
     id = params[:id].to_i
     unless session[:shopping_cart].include?(id)
       session[:shopping_cart] << id
+
       product = Product.find(id)
+
       flash[:notice] = "Added Item #{product.product_name} to Cart."
     end
     redirect_to root_path
@@ -17,7 +19,9 @@ class CartController < ApplicationController
   def destroy
     id = params[:id].to_i
     session[:shopping_cart].delete(id)
+
     product = Product.find(id)
+
     flash[:delete_notice] = "Removed Item #{product.product_name} from Cart."
     redirect_to root_path
   end
