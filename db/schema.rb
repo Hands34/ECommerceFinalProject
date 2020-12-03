602cffd365d121b22afa6752deb846464ba653e6
Mitchell Cook <m.cook2434@gmail.com>
2020-12-03 12:31:57 -0600

    added status field to orders to say whether an order was succesful or not

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_210144) do
+ActiveRecord::Schema.define(version: 2020_12_03_183046) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_210144) do
     t.integer "customer_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
